a1b12ee01dd7a8e368140f9c6bc7669fda642fdb
Adrien Nader <adrien@notk.org>
2015-05-03 10:47:05 +0200

    mingw-w64: fix version (copy-paste from gcc's description?).

--- mingw-w64/wb:headers.ml
+++ mingw-w64/wb:headers.ml
@@ -1,8 +1,8 @@
   let mingw_w64_add = add_full
     ~dir
-    ~version:"4.8.3"
+    ~version:"3.3.0"
     ~sources:[
-      Tarball ("mingw-w64-${VERSION}.tar.bz2", "d31eac960d42e791970697eae5724e529c81dcd6")
+      Tarball ("mingw-w64-v${VERSION}.tar.bz2", "d31eac960d42e791970697eae5724e529c81dcd6")
     ]
     ~outputs:[
       "${PACKAGE}-${VERSION}-${BUILD}-${HOST_TRIPLET}.txz";
